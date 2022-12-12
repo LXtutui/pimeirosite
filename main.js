af4a4df60f41b09ced047e043d7c0d8ed179e2f1
LXtutui <113383859+LXtutui@users.noreply.github.com>
2022-12-12 15:16:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-var lista_img = ["img1.png", "img2.png"];
-var indice = 0;
-function trocar_img(){
-    document.getElementById("HQ").src = lista_img[indice];
-    indice+=1;
-}
+console.log("eu sou uma tela de console");
+var primeiro_nome = "Arthur ";
+var ultimo_nome = "Camila";
+ console.log(primeiro_nome);
+ console.log(ultimo_nome);
+
+var raiz = Math.sqrt(12);
+console.log("a raiz quadrada de 12 é" + raiz);
+
+var expoente = Math.pow(10, 2);
+console.log("dez elevado a dois é: " + expoente);
